eecd33c686b588fcd424f7f7e0a0b98554cd64a0
Sergey Kuznetsov <kuznetsov.s@tengridostyk.kz>
2025-08-22 09:05:49 +0500

    Дефолтный com.example.bizlevel заменен на новое значение для публикации в Google Play

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bizlevel"
+    namespace = "kz.bizlevel.bizlevel"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
